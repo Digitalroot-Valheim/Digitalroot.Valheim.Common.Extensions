4cce389cade8d9404346a34b198ad3f0dc408f4f
Digitalroot <Digitalroot@users.noreply.github.com>
2021-07-22 22:40:18 -0700

    Update GameObjectExtensions.cs

--- src/Digitalroot.Valheim.Common.Extensions/GameObjectExtensions.cs
+++ src/Digitalroot.Valheim.Common.Extensions/GameObjectExtensions.cs
@@ -4,6 +4,30 @@ namespace Digitalroot.Valheim.Common.Extensions
 {
   public static class GameObjectExtensions
   {
+    public static GameObject AddMonoBehaviour(this GameObject gameObject, string name)
+    {
+      Type type = Assembly.GetExecutingAssembly().GetType(name) ?? MonoBehaviourRepositoryAssembly.GetType(name);
+
+      if (type == null)
+      {
+        throw new ArgumentException($"Unable to find MonoBehaviour: {name}", nameof(name));
+      }
+
+      gameObject.AddComponent(type);
+      return gameObject;
+    }
+
+    public static GameObject AddMonoBehaviour<T>(this GameObject gameObject) where T : MonoBehaviour
+    {
+      gameObject.AddComponent<T>();
+      return gameObject;
+    }
+
+    public static MonoBehaviour GetOrAddMonoBehaviour(this GameObject gameObject, string name)
+    {
+      return (gameObject.GetComponent(name) ?? gameObject.AddMonoBehaviour(name).GetComponent(name)) as MonoBehaviour;
+    }
+    
     /// <summary>
     /// Returns the component of Type type. If one doesn't already exist on the GameObject it will be added.
     /// Source: Jotunn JVL
