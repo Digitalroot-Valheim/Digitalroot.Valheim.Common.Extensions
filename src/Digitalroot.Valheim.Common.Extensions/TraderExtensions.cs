5b664f83ab7ee28c598339ca61fedd08af8a6337
Nicholas Dunnaway <nick@dunnaway.org>
2023-03-13 16:31:03 -0700

    Update for 0.214.2

---
+++ src/Digitalroot.Valheim.Common.Extensions/TraderExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Digitalroot.Valheim.Common.Extensions;
+
+public static class TraderExtensions
+{
+  public static bool IsHaldor(this Trader trader)
+  {
+    return trader != null && trader.m_name == "$npc_haldor";
+  }
+}
